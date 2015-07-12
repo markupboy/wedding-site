b07c8233efbb1bb669ee6d6c70281da4b2076936
Blake Walters <blake@markupboy.com>
2015-07-12 15:07:11 -0600

    added terraform config

---
+++ terraform/terraform.tf
@@ -0,0 +1,24 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+atlas {
+  name = "markupboy/blakeanderin.com"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+    bucket = "blakeanderin.com"
+    acl = "public-read"
+    policy = "${file("policy.json")}"
+
+    website {
+        index_document = "index.html"
+        error_document = "error.html"
+    }
+}
